cf2b611c29472568a2885f1abac342f73bd80237
Teingi <jingshun.tq@oceanbase.com>
2026-04-14 14:18:07 +0800

    v0.1.1

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['tests/**/*.test.ts'],
-    testTimeout: 10000,
-    pool: 'threads',
+    testTimeout: 10000
   },
 });
